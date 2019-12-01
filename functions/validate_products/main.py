5ec882c0be5c35e2498cff70b2e0d03aae1b5e6b
amritpal singh <amritpalsingh@amritpals-MacBook-Pro.local>
2019-12-01 00:52:50 -0500

    Implement Controller design to the processing service

--- functions/validate_products/main.py
+++ functions/validate_products/main.py
@@ -1,14 +1,10 @@
 import os
-from services.process_file import ProcessFile
+from controllers.process_file import ProcessFile
 
 process_file = ProcessFile()
+
 
 def validate_products(event, context):
     response = process_file.process_file(event)
 
-
     return "Hello World!" 
-
-
-if __name__ == "__main__":
-    process_file.process_file("")
